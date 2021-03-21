3be03e65f574e79b603844329ac9c700d35b4286
robby tong <robbytong@gmail.com>
2021-03-21 00:57:04 -0700

    accomodate new directory structure

--- Examples/Python/dfu_update.py
+++ Examples/Python/dfu_update.py
@@ -7,7 +7,7 @@ from time import sleep
 # start the sidekick up in dfu mode
 sk = SidekickIO(SidekickIO.FW_MODE_DFU)
 
-fwpath = r"../../../../Firmware/Application/helloworld/Debug/serial_bridge.bin"
+fwpath = r"../../../Firmware/Application/helloworld/Debug/serial_bridge.bin"
 
 # update the firmware and reset into application mode
 sk.firmware_update_from_file(fwpath, sk.FW_MODE_APP)
